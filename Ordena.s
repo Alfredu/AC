9dd7852403635fc92d98d54e3ea237f5a5f8508c
Aleix Sanchis Ramirez <aleix.sanchis@c6s306pc31.fib.upc.es>
2017-03-15 11:36:29 +0100

    acabados

--- Ordena.s
+++ Ordena.s
@@ -11,14 +11,58 @@ Ordenar:
 
 	# Guardamos
 	pushl %ebx
-	pushl %ecx
+	pushl %esi
+	pushl %edi
 
 	movl 8(%ebp), %ebx
-	movl $0, -8(%ebp)
-	
-	movl -8(%ebp), %ecx # ecx <- i
-	for:
-		imul $12, %ecx, %esi
-		
+	movl $0, %esi # esi <- i
 
-	# Deshacemos
+	for1:
+		imul $12, %esi, %edi # edi <- i*12
+		cmpl $0x80000000, 4(%ebx, %edi)
+		je fifor1
+		movl %esi, %eax
+		incl %eax # j <- i+1
+
+		for2:
+			pushl %eax
+
+			imul $12, %eax, %ecx
+			cmpl $0x80000000, 4(%ebx, %ecx)
+			je fifor2
+
+			movl 4(%ebx, %edi), %edx
+
+			cmpl 4(%ebx, %ecx), %edx
+			jle fi
+
+			pushl %eax
+			pushl %esi
+			pushl %ebx
+			call Intercambiar
+			addl $12, %esp
+
+			fi:
+				popl %eax
+				incl %eax
+				jmp for2
+		fifor2:
+			incl %esi
+			jmp for1
+	fifor1:
+
+		movl %esi, %eax
+
+
+		# Deshacemos
+
+		popl %edi
+		popl %esi
+		popl %ebx
+
+		movl %ebp, %esp
+		popl %ebp
+		ret
+
+
+		
