b0096c1826fb1e443b27b4273440099e77a7f5de
Aleix Sanchis Ramirez <aleix.sanchis@a5s105pc24.fib.upc.es>
2017-03-23 11:55:56 +0100

    primer simulador funcionando

--- Practica06/MiSimulador.c
+++ Practica06/MiSimulador.c
@@ -1,18 +1,17 @@
 #include "CacheSim.h"
-#include <stdio.h>
-//Escrito por Aleix Sanchis i Nicolás Francisquelo. 
 
 /* Posa aqui les teves estructures de dades globals
  * per mantenir la informacio necesaria de la cache
  * */
- struct t_linea{
+
+
+struct t_linea{
 	unsigned int tag; //Almacenamos aquí el tag de cada linia de cache
 	char valid; //Para saber si el contenido de la linea es valido;
  };
+struct t_linea cache[128]; //Nuestra cache será un array de 128 lineas. 4k / 32 bytes/linea = 128 lineas;
 
  int h; int m;
-
-struct t_linea cache[128]; //Nuestra cache será un array de 128 lineas. 4k / 32 bytes/linea = 128 lineas;
 
 /* La rutina init_cache es cridada pel programa principal per
  * inicialitzar la cache.
@@ -21,30 +20,33 @@ struct t_linea cache[128]; //Nuestra cache será un array de 128 lineas. 4k / 32
 void init_cache ()
 {
 	h=0;m=0;
-    totaltime=0.0;
 	/* Escriu aqui el teu codi */
 	int i;
 	for(i=0; i<128;i++){
 		cache[i].valid = false;
 	}
 
+
 }
 
 /* La rutina reference es cridada per cada referencia a simular */ 
-void reference (unsigned int address)
+void reference (unsigned int address, unsigned int LE)
 {
 	unsigned int byte;
 	unsigned int bloque_m; 
 	unsigned int linea_mc;
 	unsigned int tag;
-	unsigned int miss;	   // boolea que ens indica si es miss
-	unsigned int replacement;  // boolea que indica si es reempla�a una linia valida
-	unsigned int tag_out;	   // TAG de la linia reempla�ada
-	float t1,t2;		// Variables per mesurar el temps (NO modificar)
-	
-	t1=GetTime();
-	/* Escriu aqui el teu codi */
+	unsigned int miss;
+	unsigned int lec_mp;
+	unsigned int mida_lec_mp;
+	unsigned int esc_mp;
+	unsigned int mida_esc_mp;
+	unsigned int replacement;
+	unsigned int tag_out;
 
+	
+	
+	/* Escriu aqui el teu codi */
 	tag = (address & 0xFFFFF000)>>12; //Hacemos la mascara y shifteamos para cojer el TAG
 	linea_mc = (address & 0xFE0)>>5; //Lo mismo para la linea
 	byte = (address & 0x1F); //Y para el offset dentro de la linea;
@@ -56,17 +58,24 @@ void reference (unsigned int address)
 
 	miss = !linia.valid || linia.tag!=tag;
 
-	replacement = linia.valid && miss;
+	esc_mp = LE;
+	lec_mp = miss && !LE;
 
+	replacement = linia.valid && lec_mp;
+
+
+	mida_lec_mp = lec_mp*32;
+	mida_esc_mp = esc_mp;
+	
 	if(miss){
 		if(replacement){ //Si reemplazamos una linia valida
 			tag_out = linia.tag;
 		}
-		else{
+		if(lec_mp){
+			cache[linea_mc].tag = tag;
+			cache[linea_mc].valid = true;
+			m++;
 		}
-		cache[linea_mc].tag = tag;
-		cache[linea_mc].valid = true;
-		m++;
 	}
 	else{
 		h++;
@@ -74,17 +83,17 @@ void reference (unsigned int address)
 
 	/* La funcio test_and_print escriu el resultat de la teva simulacio
 	 * per pantalla (si s'escau) i comproba si hi ha algun error
-	 * per la referencia actual. Tamb� mesurem el temps d'execuci�
+	 * per la referencia actual
 	 * */
-	t2=GetTime();
-	totaltime+=t2-t1;
-	test_and_print (address, byte, bloque_m, linea_mc, tag,
-			miss, replacement, tag_out);
+	test_and_print (address, LE, byte, bloque_m, linea_mc, tag,
+			miss, lec_mp, mida_lec_mp, esc_mp, mida_esc_mp,
+			replacement, tag_out);
 }
 
 /* La rutina final es cridada al final de la simulacio */ 
 void final ()
 {
  	/* Escriu aqui el teu codi */ 
-	 printf("El numero de hits es: %d \nEl numero de misses es %d\n", h, m);
+  
+  
 }
--- Practica06/MiSimulador2.c
+++ Practica06/MiSimulador2.c
@@ -1,18 +1,18 @@
 #include "CacheSim.h"
-#include <stdio.h>
-//Escrito por Aleix Sanchis i Nicolás Francisquelo. 
 
 /* Posa aqui les teves estructures de dades globals
  * per mantenir la informacio necesaria de la cache
  * */
- struct t_linea{
+
+
+struct t_linea{
 	unsigned int tag; //Almacenamos aquí el tag de cada linia de cache
 	char valid; //Para saber si el contenido de la linea es valido;
+	char dirty; //Dancing
  };
+struct t_linea cache[128]; //Nuestra cache será un array de 128 lineas. 4k / 32 bytes/linea = 128 lineas;
 
  int h; int m;
-
-struct t_linea cache[128]; //Nuestra cache será un array de 128 lineas. 4k / 32 bytes/linea = 128 lineas;
 
 /* La rutina init_cache es cridada pel programa principal per
  * inicialitzar la cache.
@@ -21,30 +21,34 @@ struct t_linea cache[128]; //Nuestra cache será un array de 128 lineas. 4k / 32
 void init_cache ()
 {
 	h=0;m=0;
-    totaltime=0.0;
 	/* Escriu aqui el teu codi */
 	int i;
 	for(i=0; i<128;i++){
 		cache[i].valid = false;
+		cache[i].dirty = 0;
 	}
+
 
 }
 
 /* La rutina reference es cridada per cada referencia a simular */ 
-void reference (unsigned int address)
+void reference (unsigned int address, unsigned int LE)
 {
 	unsigned int byte;
 	unsigned int bloque_m; 
 	unsigned int linea_mc;
 	unsigned int tag;
-	unsigned int miss;	   // boolea que ens indica si es miss
-	unsigned int replacement;  // boolea que indica si es reempla�a una linia valida
-	unsigned int tag_out;	   // TAG de la linia reempla�ada
-	float t1,t2;		// Variables per mesurar el temps (NO modificar)
-	
-	t1=GetTime();
-	/* Escriu aqui el teu codi */
+	unsigned int miss;
+	unsigned int lec_mp;
+	unsigned int mida_lec_mp;
+	unsigned int esc_mp;
+	unsigned int mida_esc_mp;
+	unsigned int replacement;
+	unsigned int tag_out;
 
+	
+	
+	/* Escriu aqui el teu codi */
 	tag = (address & 0xFFFFF000)>>12; //Hacemos la mascara y shifteamos para cojer el TAG
 	linea_mc = (address & 0xFE0)>>5; //Lo mismo para la linea
 	byte = (address & 0x1F); //Y para el offset dentro de la linea;
@@ -52,39 +56,36 @@ void reference (unsigned int address)
 	bloque_m = address>>5;/*El bloque de memoria es la direccion menos
 							los bits de byte*/
 
-	struct t_linea linia = cache[linea_mc];
 
-	miss = !linia.valid || linia.tag!=tag;
+	miss = !cache[linea_mc].valid || cache[linea_mc].tag!=tag;
 
-	replacement = linia.valid && miss;
+	cache[linea_mc].dirty = LE && !miss;
 
-	if(miss){
-		if(replacement){ //Si reemplazamos una linia valida
-			tag_out = linia.tag;
-		}
-		else{
-		}
-		cache[linea_mc].tag = tag;
-		cache[linea_mc].valid = true;
-		m++;
-	}
-	else{
-		h++;
-	}
+	lec_mp = (miss && !LE) || (LE && miss);
+
+	replacement = cache[linea_mc].valid && miss;
+
+	esc_mp = replacement && cache[linea_mc].dirty;
+	
+	mida_lec_mp = 32*miss;
+	mida_esc_mp = esc_mp*32;
+	
+	
+
 
 	/* La funcio test_and_print escriu el resultat de la teva simulacio
 	 * per pantalla (si s'escau) i comproba si hi ha algun error
-	 * per la referencia actual. Tamb� mesurem el temps d'execuci�
+	 * per la referencia actual
 	 * */
-	t2=GetTime();
-	totaltime+=t2-t1;
-	test_and_print (address, byte, bloque_m, linea_mc, tag,
-			miss, replacement, tag_out);
+	test_and_print (address, LE, byte, bloque_m, linea_mc, tag,
+			miss, lec_mp, mida_lec_mp, esc_mp, mida_esc_mp,
+			replacement, tag_out);
 }
 
 /* La rutina final es cridada al final de la simulacio */ 
 void final ()
 {
  	/* Escriu aqui el teu codi */ 
-	 printf("El numero de hits es: %d \nEl numero de misses es %d\n", h, m);
+  
+  
 }
